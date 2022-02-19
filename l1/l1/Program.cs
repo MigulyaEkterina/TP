31d565b5f489c2671fcc3f379e2a87ff568cf6a8
MigulyaEkterina <you@example.com>
2022-02-19 16:23:26 +0300

    тест

--- l1/l1/Program.cs
+++ l1/l1/Program.cs
@@ -6,7 +6,7 @@ namespace l1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
         }
     }
 }
