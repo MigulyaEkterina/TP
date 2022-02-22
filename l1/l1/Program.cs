6ae4f5ca15d37daddc12648500196609b384b87a
MigulyaEkterina <you@example.com>
2022-02-22 10:35:33 +0300

    1

--- l1/l1/Program.cs
+++ l1/l1/Program.cs
@@ -2,11 +2,30 @@
 
 namespace l1
 {
+    class Points
+    {
+        public char a = 'c';
+        public const uint b = 9;
+        public short[] c = new short[5] {1,2,2,2,2 };
+        public enum Integers
+        {
+            one,
+            two,
+            three
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World1!");
+            Points p = new Points();
+            
+            Console.WriteLine(p.c[1]);
+            Console.WriteLine(Points.b);
+            Console.WriteLine(Points.Integers.one);
+            Console.WriteLine(Char.IsLower(p.a));
+
         }
     }
 }
